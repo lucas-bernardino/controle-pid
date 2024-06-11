1943a3fa7488c2e284bfb1ffbf0490cfb06d3d46
Lucas Gabriel Bernardino <122226174+lucas-bernardino@users.noreply.github.com>
2024-06-11 08:57:12 -0300

    Update wheel radius and format serial printing

--- possivel_hall.ino
+++ possivel_hall.ino
@@ -1,17 +1,10 @@
 #include <AFMotor.h>
 
 #define HALL_PIN 13
-#define R 0.3
+#define R 0.3429
 #define PERIMETER 2 * PI * R
 
-#define DESIRABLE_SPEED 17
-
-/*
-I put 500 on this, but I still have no idea what's the exactly number
-I created the counter variable because I think we need to have a sort of set of data before
-trying to move the motor. If we change the motor every time we get a reading, it'll probably get lost.
-*/
-#define NUMBER_OF_RECORDINGS 500 
+#define DESIRABLE_SPEED 25
 
 signed long T1 = 0;
 signed long T2 = 0;
@@ -24,13 +17,11 @@ int counter = 0;
 double integral_term = 0;
 double error_prev = 0;
 
-float kp = 1;
+float kp = 0.5;
 float ki = 0.00;
 float kd = 0.00;
 
 double pid_controller(double avg, int dt){
-  Serial.print("Avg value: ");
-  Serial.println(avg);
   double error = DESIRABLE_SPEED - avg;
   integral_term = integral_term + (error * dt);
   float derivative_term = (error - error_prev) / dt;
@@ -51,7 +42,7 @@ void setup () {
   Serial.begin(9600);
   T1 = millis();
   motor.setSpeed(90); 
-  Serial.print("Iniciando com setpoint de ");
+  Serial.print("setpont:");
   Serial.println(DESIRABLE_SPEED);
 }
 
@@ -62,21 +53,19 @@ void loop () {
     float speed = (PERIMETER / time_seconds) * 1000 * 3.6;
     counter+=1;
     if (speed < 50) {
+      Serial.print("tempo:");
+      Serial.println(time_seconds);
+      Serial.print("velocidade_atual:");
       Serial.println(speed);
-    }
-    double step = pid_controller(speed, time_seconds);
-    Serial.print("PID_CONTROLLER OUTPUT: ");
-    Serial.println(step);
-    Serial.print("\n");
-    if (speed_sum > DESIRABLE_SPEED) {
-      Serial.println("Starting backwards"); 
-      motor.step(abs(step), FORWARD, MICROSTEP);
-      Serial.println("Stoping backwards");
-    }
-    if (speed_sum < DESIRABLE_SPEED) {
-      Serial.println("Starting forward");
-      motor.step(abs(step), BACKWARD, MICROSTEP);
-      Serial.println("Stoping forward");
+      double step = pid_controller(speed, time_seconds);
+      Serial.print("pid_output:");
+      Serial.println(step);
+      if (speed_sum > DESIRABLE_SPEED) {
+        motor.step(abs(step), FORWARD, MICROSTEP);
+      }
+      if (speed_sum < DESIRABLE_SPEED) {
+        motor.step(abs(step), BACKWARD, MICROSTEP);
+      }
     }
     T1 = T2;
   }
