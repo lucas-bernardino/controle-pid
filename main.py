6e99a847a980e36242fb9bc93a2a0c4306ac2fe2
lucas-bernardino <lucasgber@gmail.com>
2024-07-11 11:57:18 -0300

    Add code to interact and save data from the i2c temperature sensor

--- main.py
+++ main.py
@@ -7,32 +7,45 @@ from time import sleep
 
 from datetime import datetime
 
-ser = serial.Serial(port='COM3', baudrate=9600) 
+ser_pid = serial.Serial(port='COM4', baudrate=9600) 
+ser_temp = serial.Serial(port='COM5', baudrate=9600)
+
+flag_vel = False
+flag_pid = False
+flag_temp = False
 
 def save_serial_data(path):
     file = open(path, "w")
-    flag_vel = False
-    flag_pid = False
-    file.write("velocidade,pid") # Colunas
+    file.write("velocidade,pid,temp") # Colunas
     file.write("\n")
 
     while True:
-        data = str(ser.readline())
-        print(data)
-        if "velocidade" in data:
-            _, vel = data.split(":")
+        global flag_vel, flag_pid, flag_temp
+
+        data_pid = str(ser_pid.readline())
+        data_temp = str(ser_temp.readline())
+
+        if "velocidade" in data_pid:
+            _, vel = data_pid.split(":")
             vel = vel[:-5]
             flag_vel = True
-        if "pid" in data:
-            _, pid = data.split(":")
+        if "pid" in data_pid:
+            _, pid = data_pid.split(":")
             pid = pid[:-5]
             flag_pid = True
-        if flag_vel == True and flag_pid == True:
+        if "Temperatura" in data_temp:
+            _, temp = data_temp.split(":")
+            temp = temp[:-5]
+            flag_temp = True
+        
+        if flag_vel == True and flag_pid == True and flag_temp == True:
             flag_vel = False
             flag_pid = False
-            file.write(f"{vel},{pid}") 
+            flag_temp = False
+            file.write(f"{vel},{pid},{temp}") 
             file.write("\n")
-            print(f"Tempo: {datetime.now()} | Velocidade: {vel} | PID: {pid}")
+            print(f"Tempo: {datetime.now()} | Velocidade: {vel} | PID: {pid} | Temperatura: {temp}")
+
 
 def plot_data(path, setpoint):
     df = pd.read_csv(path)
@@ -40,27 +53,28 @@ def plot_data(path, setpoint):
     erro = [(x - setpoint) for x in velocidade]
     pid = df["pid"].to_numpy()
     tempo = np.arange(0, len(velocidade))
-    print(velocidade)
+    temp = df["temp"].to_numpy()
     plt.plot(tempo, velocidade, label="Velocidade")
     plt.plot(tempo, erro, label="Erro")
     plt.plot(tempo, pid, label="Pid")
+    plt.plot(tempo, temp, label="Temperatura")
     plt.yticks(np.arange(min(erro), max(velocidade)))
     plt.legend()
     plt.show()
 
 def init_parameters(setpoint, kp, ki, kd):
     while True:
-        is_ready = str(ser.readline())
+        is_ready = str(ser_pid.readline())
         if "Ready" in is_ready:
             while True:
-                if "setup_completed" in str(ser.readline()):
+                if "setup_completed" in str(ser_pid.readline()):
                     print("Parameters initialized successfully.")
                     return
-                ser.write(f"{setpoint}S".encode())
-                ser.write(f"{kp}P".encode())
-                ser.write(f"{ki}I".encode())
-                ser.write(f"{kd}D".encode())
-                                            
-init_parameters("35", "0.6", "0.00000018", "0.00000045")
-save_serial_data("setpoint14_10;44.csv")
-#plot_data("setpoint14_10;44.csv", 35)
+                ser_pid.write(f"{setpoint}S".encode())
+                ser_pid.write(f"{kp}P".encode())
+                ser_pid.write(f"{ki}I".encode())
+                ser_pid.write(f"{kd}D".encode())
+
+init_parameters("17", "0.6", "0.00000018", "0.00000045")
+save_serial_data("setpoint17_1238.csv")
+#plot_data("setpoint17_1238.csv", 17)
